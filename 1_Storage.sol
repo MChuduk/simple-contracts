fa73b164bd43f97eaccf0b96bbe975c0fb4fdc34
MChuduk <mihail2014mihail@gmail.com>
2023-06-20 21:18:28 +0300

    added salaries

--- 1_Storage.sol
+++ 1_Storage.sol
@@ -5,12 +5,19 @@ contract SimpleStorage {
 
     Person[] public employees;
 
+    mapping(string => uint256) salaries;
+
     struct Person {
         string name;
         uint256 age;
     }
 
-    function addPerson(string memory _name, uint256 _age) public {
+    function employeeSalary(string memory _name) public view returns(uint256) {
+        return salaries[_name];
+    }
+
+    function addEmployee(string memory _name, uint256 _age, uint256 _salary) public {
         employees.push(Person(_name, _age));
+        salaries[_name] = _salary;
     }
 }
