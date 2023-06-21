0c18d370637e24b28bd4bbd747a974bc08e3e410
MChuduk <mihail2014mihail@gmail.com>
2023-06-21 10:55:08 +0300

    added storageFactory

---
+++ 2_StorageFactory.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+import "./1_Storage.sol";
+
+contract StorageFactory {
+
+    SimpleStorage[] public simpleStorageList;
+
+    function createStorage() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageList.push(simpleStorage);
+    }
+
+    function addEmployee(uint256 _storageIndex, string memory _name, uint256 _age, uint256 _salary) public  {
+        SimpleStorage simpleStorage = simpleStorageList[_storageIndex];
+        simpleStorage.addEmployee(_name, _age, _salary);
+    }
+
+    function getEmployeeSalary(uint256 _storageIndex, string memory _name) public view returns (uint256) {
+        SimpleStorage simpleStorage = simpleStorageList[_storageIndex];
+        return simpleStorage.employeeSalary(_name);
+    }
+}
