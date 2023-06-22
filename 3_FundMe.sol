7468479312c303aebde01b9ec4ea1a2c7a03a8ba
MChuduk <mihail2014mihail@gmail.com>
2023-06-22 11:33:27 +0300

    added modifier for contract owner

--- 3_FundMe.sol
+++ 3_FundMe.sol
@@ -9,11 +9,32 @@ contract FundMe {
     uint256 public constant MIN_USD = 50 * 1e18;
 
     address[] funders;
-    mapping (address => uint256) addressToFoundingAmount;
+    mapping (address => uint256) public addressToFoundingAmount;
 
-    function fundMe() public payable  {
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function fundMe() public payable {
         require(msg.value.getConversationRate() >= MIN_USD, "Your funding should be >= 50 USD");
         funders.push(msg.sender);
-        addressToFoundingAmount[msg.sender] = msg.value;
+        addressToFoundingAmount[msg.sender] += msg.value;
+    }
+
+    modifier onlyOnwer() {
+        require(owner == msg.sender, "This function can be callled only for contract owner");
+        _;
+    }
+
+    function widthdraw() public onlyOnwer {
+        for(uint256 i = 0; i < funders.length; i++) {
+            address funder = funders[i];
+            addressToFoundingAmount[funder] = 0;
+        }
+        funders = new address[](0);
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
     }
 }
